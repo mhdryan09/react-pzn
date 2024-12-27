22a040644149982eb21a20f86861c9d7fc62210a
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-27 22:40:18 +0700

    Style

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,3 +1,5 @@
+import "./HelloWorld.css";
+
 export default function HelloWorld() {
 	const props = {
 		text: "Hello World from Spread Operator",
@@ -11,23 +13,10 @@ export default function HelloWorld() {
 }
 
 function HeaderHelloWorld({ text = "Ups lupa kasih text" }) {
-	return (
-		<h1
-			style={{
-				color: "red",
-				backgroundColor: "aqua",
-			}}
-		>
-			{text.toUpperCase()}
-		</h1>
-	);
+	return <h1 className="title">{text.toUpperCase()}</h1>;
 }
 
 function ParagraphHelloWorld() {
 	const text = "This is a simple component";
-	const style = {
-		color: "blue",
-		backgroundColor: "yellow",
-	};
-	return <p style={style}>{text.toLowerCase()}</p>;
+	return <p className="content">{text.toLowerCase()}</p>;
 }
