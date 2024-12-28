7b4d1d0b00576c1137471f02b26287f05824b353
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-28 08:58:12 +0700

    Event Handler

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -4,6 +4,8 @@ import HelloWorld from "./HelloWorld";
 import Container from "./Container";
 import TodoList from "../todolist/TodoList";
 import Table from "../table/Table";
+import AlertButton from "../button/AlertButton";
+import MyButton from "../button/MyButton";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
@@ -11,6 +13,9 @@ createRoot(document.getElementById("root")).render(
 			<HelloWorld />
 			<TodoList />
 			<Table />
+
+			<AlertButton text="Click Me" message={"Hello World"} />
+			<MyButton text="Hit Me" onSmash={() => alert("Smashed")} />
 		</Container>
 	</StrictMode>,
 );
