7e39bfff82c0747f7ae7409404c44beabe5daf95
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-28 08:37:19 +0700

    Pure Component

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
 import Container from "./Container";
 import TodoList from "../todolist/TodoList";
+import Table from "../table/Table";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Container>
 			<HelloWorld />
 			<TodoList />
+			<Table />
 		</Container>
 	</StrictMode>,
 );
