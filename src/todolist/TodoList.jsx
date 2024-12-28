798727b348273e146a40c75c40a5200456b79639
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-28 08:24:56 +0700

    Collection Component

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,12 +1,34 @@
 import Todo from "./Todo";
 
 export default function TodoList() {
+	const data = [
+		{
+			id: 1,
+			text: "Learn HTML",
+			isCompleted: true,
+		},
+		{
+			id: 2,
+			text: "Learn CSS",
+			isCompleted: true,
+		},
+		{
+			id: 3,
+			text: "Learn Javascript",
+			isCompleted: true,
+		},
+		{
+			id: 4,
+			text: "Learn React",
+			isCompleted: false,
+		},
+	];
+
 	return (
 		<ul>
-			<Todo isCompleted={true} text="Learn HTML" isDeleted={true} />
-			<Todo isCompleted={true} text="Learn CSS" />
-			<Todo isCompleted={true} text="Learn Javascript" />
-			<Todo isCompleted={false} text="Learn React" />
+			{data.map(todo => (
+				<Todo {...todo} key={todo.id} />
+			))}
 		</ul>
 	);
 }
